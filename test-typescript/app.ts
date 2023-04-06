bd77f04386a633368db520a9c2d504587a418e43
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-04-06 14:13:29 -0400

    Practice objs, arrays, enums

--- test-typescript/app.ts
+++ test-typescript/app.ts
@@ -1,10 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
-  name: 'Maximilian',
-  age: 30
-};
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
 
-console.log(person.name);
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
