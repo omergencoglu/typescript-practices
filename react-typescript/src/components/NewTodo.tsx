165f18e80dabe981c77849ea2f1a48b348093946
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-08-17 05:27:01 -0400

    Add delete option

--- react-typescript/src/components/NewTodo.tsx
+++ react-typescript/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 
+import "./NewTodo.css";
+
 type NewTodoProps = { onAddTodo: (todoText: string) => void };
 
 const NewTodo: React.FC<NewTodoProps> = (props) => {
@@ -13,7 +15,7 @@ const NewTodo: React.FC<NewTodoProps> = (props) => {
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" ref={textInputRef} />
       </div>
