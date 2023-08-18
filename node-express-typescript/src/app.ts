8c45e71a0e2f3575c94b29df757580991b934889
Omer Gencoglu <omersaitgencoglu@gmail.com>
2023-08-18 16:08:16 -0400

    Add more CRUD operations

--- node-express-typescript/src/app.ts
+++ node-express-typescript/src/app.ts
@@ -9,10 +9,8 @@ app.use(json());
 
 app.use("/todos", todoRoutes);
 
-app.use(
-  (err: Error, req: express.Request, res: Response, next: NextFunction) => {
-    res.status(500).json({ message: err.message });
-  }
-);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
